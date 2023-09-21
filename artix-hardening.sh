19f95e2b23506ffa973cc5ec2a17a8fc41a55631
Emil8630 <DonaldWillis87@protonmail.com>
2023-09-21 15:56:43 +0200

    Update artix-hardening.sh

--- artix-hardening.sh
+++ artix-hardening.sh
@@ -9,26 +9,26 @@ kernel_version=$(uname -r)
 usr=$(whoami)
 
 if [ "$kernel_version" = *"arch"* ]; then
-    sudo pacman -S --noconfirm ufw fail2ban netstat
+    pacman -S --noconfirm ufw fail2ban netstat
 else
-    sudo apt install -y ufw fail2ban
+    apt install -y ufw fail2ban
 fi
 
 # --- Setup UFW rules
-sudo ufw limit 22/tcp  
-sudo ufw allow 80/tcp  
-sudo ufw allow 443/tcp  
-sudo ufw default deny incoming  
-sudo ufw default allow outgoing
-sudo ufw enable
+ufw limit 22/tcp  
+ufw allow 80/tcp  
+ufw allow 443/tcp  
+ufw default deny incoming  
+ufw default allow outgoing
+ufw enable
 
 # --- Harden /etc/sysctl.conf
-sudo sysctl kernel.modules_disabled=1
-sudo sysctl -a
-sudo sysctl -A
-sudo sysctl mib
-sudo sysctl net.ipv4.conf.all.rp_filter
-sudo sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
+sysctl kernel.modules_disabled=1
+sysctl -a
+sysctl -A
+sysctl mib
+sysctl net.ipv4.conf.all.rp_filter
+sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
 
 # --- PREVENT IP SPOOFS
 cat <<EOF > /etc/host.conf
@@ -37,10 +37,10 @@ multi on
 EOF
 
 # --- Enable fail2ban
-sudo -U $usr cp $HOME/github/suckless/jail.local /etc/fail2ban/
-sudo ln -s /etc/runit/sv/fail2ban /etc/service/
-sudo sv start fail2ban
+cp $HOME/github/suckless/jail.local /etc/fail2ban/
+ln -s /etc/runit/sv/fail2ban /etc/service/
+sv start fail2ban
 
 echo "listening ports"
-sudo netstat -tunlp
+netstat -tunlp
 
